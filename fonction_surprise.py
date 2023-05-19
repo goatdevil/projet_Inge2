5e9c3d0e236dc5fae350815001b9fcd433f63d8a
GdePonthaud <127596326+goatdevil@users.noreply.github.com>
2023-05-19 10:19:03 +0200

    Update fonction_surprise.py

--- fonction_surprise.py
+++ fonction_surprise.py
@@ -4,13 +4,15 @@ from surprise import Dataset
 from surprise.model_selection.search import GridSearchCV
 import pandas as pd
 import random
-
+from textblob import TextBlob
+import csv
 
 
 reader=Reader(rating_scale=(-1,1))
 df=pd.read_csv("surprise.csv")
 df2=pd.read_csv("blob.csv")
-dico_user={}
+dico_user_id={}
+dico_id_user={}
 compteur=0
 
 column=list(df2.columns)
@@ -18,14 +20,17 @@ column.pop(0)
 
 for user in column:
     compteur+=1
-    dico_user[user]=compteur
+    dico_user_id[user]=compteur
+    dico_id_user[compteur]=user
 
 liste_hotel=list(df2["Unnamed: 0"].values)
-dico_hotel={}
+dico_hotel_id={}
+dico_id_hotel={}
 compteur=0
 for hotel in liste_hotel:
     compteur+=1
-    dico_hotel[compteur]=hotel
+    dico_id_hotel[compteur]=hotel
+    dico_hotel_id[hotel]=compteur
 
 def création_fit_model(df):
     data = Dataset.load_from_df(df[["User_id", "Hotel_id", "Sentiment"]], reader)
@@ -73,10 +78,31 @@ def generate_recommendation(nom_user, model, metadata, thresh=0.45):#generation 
 def affichage_recommendation(hotel_comm,hotel_reco):
     nom_hotel_comm = []
     for indice in hotel_comm:  # On utilise le dictionnaire id_hotel=> nom_hotel pour afficher le nom des hotels ou la personne à laisser un commentaire
-        nom_hotel_comm.append(dico_hotel[indice])
+        nom_hotel_comm.append(dico_id_hotel[indice])
     print(nom_hotel_comm)
 
     nom_hotel_reco = []
     for indice in hotel_reco:  # Pareil pour les hotels que l'on recommande
-        nom_hotel_reco.append(dico_hotel[indice])
+        nom_hotel_reco.append(dico_id_hotel[indice])
     print(nom_hotel_reco)
+    return
+
+def ajout_new_comm(newdf,nom_user,nom_hotel,comm):
+    id_user=dico_user_id[nom_user]
+    id_hotel=dico_id_hotel[nom_hotel]
+    ajout_df=pd.DataFrame({"User_id":[id_user],"Hotel_id":[id_hotel],"Sentiment":[TextBlob(comm).sentiment.polarity]})
+    newdf=pd.concat(newdf,ajout_df)
+    return newdf
+
+def find_comm (nom_user):
+    id_user=dico_user_id[nom_user]
+    tab_comm=[]
+    with open('df_commentaire_sans_caracteres_speciaux.csv', 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        header = next(reader)
+        row=reader[id_user]
+        for comm in row:
+            if comm!="":
+                tab_comm.append(comm)
+    return tab_comm
+
